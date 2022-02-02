4dd7e3900069a05ab0180d07fb65911f296bfded
srinivasbanala10 <98865131+srinivasbanala10@users.noreply.github.com>
2022-02-02 10:43:27 +0530

    Delete money-transfer-master directory

--- money-transfer-master/src/main/kotlin/com/money/transfer/exceptions/Exceptions.kt
+++
@@ -1,7 +0,0 @@
-package com.money.transfer.exceptions
-
-open class AccountServiceException(override val message: String) : RuntimeException()
-
-class AccountNotFoundException: AccountServiceException("Account not found")
-
-class InsufficientFundsException: AccountServiceException("Insufficient funds")
--- money-transfer-master/target/classes/com/money/transfer/repository/AccountRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.money.transfer.repository
-
-import com.money.transfer.model.Account
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface AccountRepository : JpaRepository<Account, Long> {
-    fun getById(id: Long): Optional<Account>
-}
